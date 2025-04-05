c0c07f233c173223eca0b11243367fbd1a921cd3
SelvaGeetha <92872009+SelvaGeetha@users.noreply.github.com>
2025-04-05 22:49:33 +0530

    Add files via upload

---
+++ PrintHelloandName.cs
@@ -0,0 +1,17 @@
+
+// Write a C# Sharp program to print Hello and your name in a separate line.
+// Expected Output :
+// Hello: Alexandra Abramov
+
+public class Exercise1
+{
+    // This is the main method where the program execution starts
+    public static void Main()
+    {
+        // This line outputs "Hello" to the console
+        System.Console.WriteLine("Hello");
+
+        // This line outputs "Alexandra Abramov!" to the console
+        System.Console.WriteLine("Alexandra Abramov!");
+    }
+}
