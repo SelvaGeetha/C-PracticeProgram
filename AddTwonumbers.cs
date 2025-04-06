6a2115ed04487e3f273c6ab84653ea59db5fbb25
SelvaGeetha <92872009+SelvaGeetha@users.noreply.github.com>
2025-04-06 22:39:10 +0530

    Add files via upload

---
+++ AddTwonumbers.cs
@@ -0,0 +1,12 @@
+// 2. Sum of Two Numbers
+
+// Write a C# Sharp program to print the sum of two numbers.
+public class Exercise2
+{
+    // This is the main method where the program execution starts
+    public static void Main()
+    {
+        // This line performs addition of 15 and 17 and outputs the result to the console
+        System.Console.WriteLine(15 + 17);
+    }
+}
