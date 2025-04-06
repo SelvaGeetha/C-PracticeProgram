475decf3610b8dc963ce9856aef171d296bdea29
SelvaGeetha <92872009+SelvaGeetha@users.noreply.github.com>
2025-04-06 22:48:19 +0530

    Add files via upload

---
+++ DivideTwoNumbers.cs
@@ -0,0 +1,14 @@
+// 3. Divide Two Numbers
+
+// Write a C# Sharp program to print the result of dividing two numbers.
+
+// This is the beginning of the Exercise3 class
+public class Exercise3
+{
+    // This is the main method where the program execution starts
+    public static void Main()
+    {
+        // This line performs division of 36 by 6 and outputs the result to the console
+        System.Console.WriteLine(36 / 6);
+    }
+}
