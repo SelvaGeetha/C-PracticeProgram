e01c985a9a0b468ca379ebf3e9998a4be8566977
SelvaGeetha <92872009+SelvaGeetha@users.noreply.github.com>
2025-04-07 21:51:31 +0530

    Add files via upload

---
+++ MultiplyThreeNumbers.cs
@@ -0,0 +1,44 @@
+// 6. Multiply Three Numbers
+
+// Write a C# Sharp program to print the output of the multiplication of three numbers entered by the user.
+// Test Data:
+// Input the first number to multiply: 2
+// Input the second number to multiply: 3
+// Input the third number to multiply: 6
+// Expected Output:
+// 2 x 3 x 6 = 36
+
+using System;
+
+// This is the beginning of the Exercise6 class
+public class Exercise6
+{
+    // This is the main method where the program execution starts
+    public static void Main()
+    {
+        // Declaration of variables to store three numbers
+        int num1, num2, num3;
+
+        // Prompting the user to input the first number to multiply
+        Console.Write("Input the first number to multiply: ");
+        // Reading the first number entered by the user and converting it to an integer
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        // Prompting the user to input the second number to multiply
+        Console.Write("Input the second number to multiply: ");
+        // Reading the second number entered by the user and converting it to an integer
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Prompting the user to input the third number to multiply
+        Console.Write("Input the third number to multiply: ");
+        // Reading the third number entered by the user and converting it to an integer
+        num3 = Convert.ToInt32(Console.ReadLine());
+
+        // Calculating the result of multiplying the three numbers
+        int result = num1 * num2 * num3;
+
+        // Displaying the multiplication formula and the result to the console
+        Console.WriteLine("Output: {0} x {1} x {2} = {3}",
+                            num1, num2, num3, result);
+    }
+}
